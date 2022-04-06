a82e2810c8497f548981aa973d3664df6896cec5
Joseph May <josephmay@josephs-mbp.attlocal.net>
2022-04-06 15:00:10 -0400

    put in router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+
   );
 }
 
