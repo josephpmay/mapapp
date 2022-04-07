af92eb8719570f44cf34dfa7acc13a8b611a3bbf
Joseph May <josephmay@josephs-mbp.attlocal.net>
2022-04-07 14:23:39 -0400

    implemented switch

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ParcelsList from './ParcelsList';
 
 const Home = () => {
+    const [switchval, setSwitchval] = useState(1);
+
+    function switchList() {
+        switch (switchval) {
+            case 1:
+                return <ParcelsList /> ;
+            case 2:
+                return <h1>2</h1>
+            case 3:
+                return <h1>3</h1>
+             default:
+                 return <ParcelsList />
+        }
+    }
+
     return ( 
         <div className="homecontent">
             <h1>Home Page</h1>  
-            <ParcelsList />  
+            <button className="switchbutton" onClick={ () => setSwitchval(1) }>Parcels</button>
+            <button className="switchbutton" onClick={ () => setSwitchval(2) }>Buildings</button>
+            <button className="switchbutton" onClick={ () => setSwitchval(3) }>Units</button>
+            <div className="listing">
+               { switchList() } 
+            </div>
+
         </div>
     );
 }
